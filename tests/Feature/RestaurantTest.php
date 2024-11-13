96542c2c6b0e5e627b43ff630e66555f9ac9d7a6
k-nosung <tiroruporo.love@gmail.com>
2024-11-13 22:29:07 +0900

    会員側の店舗詳細ページを作ろう

--- tests/Feature/RestaurantTest.php
+++ tests/Feature/RestaurantTest.php
@@ -41,4 +41,27 @@ class RestaurantTest extends TestCase
 
         $response->assertRedirect(route('admin.home'));
     }
-}
+
+    public function test_guest_can_access_restaurant_show_page()
+    {
+        $restaurant = Restaurant::factory()->create();
+        $response = $this->get(route('restaurants.show', $restaurant));
+        $response->assertOk();
+    }
+
+    public function test_authenticated_user_can_access_restaurant_show_page()
+    {
+        $user = User::factory()->create();
+        $restaurant = Restaurant::factory()->create();
+        $response = $this->actingAs($user)->get(route('restaurants.show', $restaurant));
+        $response->assertOk();
+    }
+    
+    public function test_admin_cannot_access_restaurant_show_page()
+    {
+        $admin = Admin::factory()->create();
+        $this->actingAs($admin, 'admin');
+        $response = $this->get('/restaurants'); 
+        $response->assertRedirect(route('admin.home'));
+    }
+ }
