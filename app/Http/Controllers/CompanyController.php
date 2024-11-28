e8ba4aeaccb201bc6e73b2f27be393a667d04f24
k-nosung <tiroruporo.love@gmail.com>
2024-11-29 00:03:58 +0900

    課題：会員側の基本情報表示機能を作ろう

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Company;
+
+class CompanyController extends Controller
+{
+      // -----
+    // index アクション
+    // -----
+    public function index() {
+        $company = Company::first();
+        return view('company.index', compact('company'));
+     }
+}
